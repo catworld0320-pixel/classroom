6cb749a3ae3e4204b5db31d9d52a918199501466
catworld0320-pixel <catworld0320@gmail.com>
2026-09-24 23:44:47 +0900

    Create model.js

---
+++ money/joseon/model.js
@@ -0,0 +1,56 @@
+const dashboardData = {
+  payday: 24,
+  periods: [
+    {
+      period: "09.24 ~ 10.24",
+      salary: "150,000원",
+      note: "진행 중인 정산 기간",
+      completed: [
+        "장구",
+        "5첩밥상 그릇 6개",
+        "4가지 깃발",
+        "도자기막걸리그릇+병",
+        "꽹과리",
+      ],
+      inProgress: [
+        { name: "북", percent: 0 },
+        { name: "징", percent: 0 },
+        { name: "태평소", percent: 0 },
+        { name: "곰방대", percent: 0 },
+        { name: "부채", percent: 0 },
+        { name: "육모방망이", percent: 0 },
+        { name: "호각", percent: 0 },
+        { name: "등체", percent: 0 },
+        { name: "책", percent: 0 },
+      ]
+    },
+    {
+      period: "08.24 ~ 09.24",
+      salary: "",
+      note: "",
+      completed: [
+        "",
+        "",
+        ""
+      ],
+      inProgress: []
+    },
+    {
+      period: "07.24 ~ 08.24",
+      salary: "",
+      note: "",
+      completed: [
+        "",
+        ""
+      ],
+      inProgress: []
+    },
+    {
+      period: "06.24 ~ 07.24",
+      salary: "",
+      note: "",
+      completed: [],
+      inProgress: []
+    }
+  ]
+};
